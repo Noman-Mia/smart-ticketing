44640eb6a206196ef845149cb64ac00c36757f09
Noman Mia <nomanislamnishad@gmail.com>
2024-02-19 11:10:17 +0600

    when click sit will be pluse added

--- js/app-1.js
+++ js/app-1.js
@@ -6,6 +6,24 @@ for (let index = 0; index < allBtns.length; index++) {
   console.log(btnName);
 }
 
+// every click 1 will be (+)
+const allButton =document.getElementsByClassName("btns"); 
+let count= 0;
+for(const btn of allButton){
+    btn.addEventListener('click',function (e){
+        count=count+1;
+        document.getElementById("incress").innerText=count;
+    })
+}
+// every click 1 will be (-)
+const allBtn =document.getElementsByClassName("btns"); 
+let cont= 8;
+for(const btn of allBtn){
+    btn.addEventListener('click',function (e){
+        cont=cont-1;
+        document.getElementById("dicress").innerText=cont;
+    });
+}
 
 function addToCard(sitname) {
   const cartItem = document.createElement("tr");
